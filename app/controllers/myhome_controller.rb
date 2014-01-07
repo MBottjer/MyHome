85f16bd88a08f2c889cb5be04fa45ae88a987446
Michael Bottjer <michaelbottjer@hotmail.com>
2014-01-07 15:38:29 +0000

    now have a specified video embedded on homepage

--- app/controllers/myhome_controller.rb
+++ app/controllers/myhome_controller.rb
@@ -4,7 +4,8 @@ class MyhomeController < ApplicationController
 
     if current_user
       client = YouTubeIt::Client.new(:username => YouTubeITConfig.username , :password => YouTubeITConfig.password , :dev_key => YouTubeITConfig.dev_key)
-      @videos = client.videos_by(:query => "bon iver", :duration => "long")
+      @videos = client.videos_by(:query => "bon iver album", :per_page => 2)
+      # @videos = client.playlists(username, "music")
       @links = current_user.links
 
       respond_to do |format|
