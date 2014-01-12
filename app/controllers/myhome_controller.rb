2c047b3d59c8673f9ac725b4ecc3520ffc7141f1
Michael Bottjer <michaelbottjer@hotmail.com>
2014-01-12 23:37:23 +0000

    refactored further to remove clutter from home controller

--- app/controllers/myhome_controller.rb
+++ app/controllers/myhome_controller.rb
@@ -3,27 +3,13 @@ class MyhomeController < ApplicationController
   def index
 
     if current_user
-      @artists = current_user.artists
-      @videos = Youtube.filter_videos(@artists)
-      # @videos = client.videos_by(:query => iterate_through(videos), :per_page => 2)
-      # @videos = client.playlists(username, "music")
-      @links = current_user.links
       @user = current_user
-      @information = information.inject(Hash.new(0)) {|hash, number| hash[number] += 1 ; hash}
+      @artists = @user.artists
+      @videos = Youtube.filter_videos(@artists)
+      @links = @user.links
+      @information = Github.create_hash_of_dominant_languages_in_repo_from(Github.dominantly_used_languages)
     end
 
   end
-
-
-  def get_json(uri)
-    response = Net::HTTP.get_response(URI(uri))
-    JSON.parse(response.body)
-  end
-
-  def information 
-    get_json("https://api.github.com/users/Mbottjer/repos").map {|repo| repo["language"]}
-  end
-
-
 
 end
---
+++ app/models/github.rb
@@ -0,0 +1,15 @@
+class Github < ActiveRecord::Base
+
+  def self.get_json(uri)
+    response = Net::HTTP.get_response(URI(uri))
+    JSON.parse(response.body)
+  end
+
+  def self.dominantly_used_languages 
+    get_json("https://api.github.com/users/Mbottjer/repos").map {|repo| repo["language"]}
+  end
+
+  def self.create_hash_of_dominant_languages_in_repo_from(used_languages)
+    used_languages.inject(Hash.new(0)) {|hash, number| hash[number] += 1 ; hash}
+  end
+end
