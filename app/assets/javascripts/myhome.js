f477c6e27cbea52ef637608546b0ca0b0439d9e9
Michael Bottjer <michaelbottjer@hotmail.com>
2014-01-17 15:46:43 +0000

    added in method for sorting via date of repo creation

--- app/assets/javascripts/myhome.js
+++ app/assets/javascripts/myhome.js
@@ -9,20 +9,40 @@ $(document).ready(function(){
   });
 });
 
-$("#btn_get_repos").click(function() {
+var sort_by = function(field, reverse, primer){
+   var key = function (x) {return primer ? primer(x[field]) : x[field]};
+
+   return function (a,b) {
+    var A = key(a), B = key(b);
+    return ( (A < B) ? -1 : ((A > B) ? 1 : 0) ) * [-1,1][+!!reverse];                  
+   }
+}
+
+
+
+$("#my_repos").click(function() {
     $.ajax({
         type: "GET",
         url: "https://api.github.com/users/MBottjer/repos",
         dataType: "json",
         success: function(result) {
-            for( i in result ) {
+            // result.sort(function(a,b){ return Date.parse(a.pushed_at) - Date.parse(b.pushed_at)})
+            result.sort(sort_by('created_at', false, Date.parse));
+
+            // result is an array of objects
+            for( object in result ) {
+              // iterate through array of objects choosing specific object via normal array method
+              console.log(result[object])
+              console.log(result[object].created_at > "2013-12-01T00:00:00Z" )
+              // potentially could limit the repos shown as done below
+              if (result[object].created_at >= "2013-11-20T00:00:00Z"){
                 $("#repo_list").append(
-                    "<li><a href='" + result[i].html_url + "' target='_blank'>" +
-                    result[i].name + "</a></li>"
+                    "<li id='repos_list'><a href='" + result[object].html_url + "' target='_blank'>" +
+                    result[object].name + "</a></li>"
                 );
-                console.log("i: " + i);
+              }
             }
-            console.log(result);
+
             $("#repo_count").append("Total Repos: " + result.length);
         }
     });
