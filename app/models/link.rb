f3ae513f7d103accd881288cd318e6d05715968f
Michael Bottjer <michaelbottjer@hotmail.com>
2014-01-24 15:53:35 +0000

    refactored long if statement for link having relevant icon

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,22 +1,12 @@
 class Link < ActiveRecord::Base
   belongs_to :user
 
-  def has_relevant_icon 
-    if self.name.match /^[gG]ithub/
-      "fa fa-github-square fa-2x"
-    elsif self.name.match /^[fF]acebook/
-      "fa fa-facebook-square fa-2x"
-    elsif self.name.match /^[yY]outube/
-      "fa fa-youtube fa-2x"
-    elsif self.name.match /^[gG]oogle+/
-      "fa fa-google-plus fa-2x"
-    elsif self.name.match /^[lL]inkedin/
-      "fa fa-linkedin-square fa-2x"
-    elsif self.name.match /^[tT]witter/
-      "fa fa-twitter fa-2x" 
-    else 
-      "button radius"
-    end
-  end
+  ICONS = { /^[gG]ithub/ => "fa fa-github-square fa-2x", /^[fF]acebook/ => "fa fa-facebook-square fa-2x", /^[yY]outube/ => "fa fa-youtube fa-2x", /^[gG]oogle+/ => "fa fa-google-plus fa-2x", /^[lL]inkedin/ => "fa fa-linkedin-square fa-2x", /^[tT]witter/ => "fa fa-twitter fa-2x"}
+
+  def relevant_icon
+    ICONS.each_key { |key| self.name.match(key) ? (return ICONS[key]) : "button radius"}
+  end 
+
+
 
 end
